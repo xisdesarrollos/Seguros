6d3d5514f00ba5f01124c237c6c1970f3ff0798f
xisdesarrollos <rescamilla@xis.mx>
2015-11-02 18:10:56 -0600

    eliminar cliente

--- Seguros American/Forms/Clientes/FrmGestionClientes.cs
+++ Seguros American/Forms/Clientes/FrmGestionClientes.cs
@@ -12,16 +12,40 @@ namespace Seguros_American.Forms.Clientes
 {
     public partial class FrmGestionClientes : Form
     {
+        private String sqlSelect = "SELECT * FROM clientes ORDER BY nombre ASC";
+
         public FrmGestionClientes()
         {
             InitializeComponent();
-            String sqlSelect = "SELECT * FROM clientes ORDER BY nombre ASC";
+            
             Globales.cargaGrid(sqlSelect, dgvClientes);
         }
 
         private void btnNuevo_Click(object sender, EventArgs e)
         {
+            Globales.EsNuevoCliente = true;
+            FrmNuevoCliente nuevocliente = new FrmNuevoCliente();
+            nuevocliente.ShowDialog();
+        }
 
+        private void dgvClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void btnEliminarCli_Click(object sender, EventArgs e)
+        {
+            int index = dgvClientes.CurrentCell.RowIndex;
+            DataGridViewRow selectedRow = dgvClientes.Rows[index];
+            string idCliente = selectedRow.Cells[0].Value.ToString();
+
+            Basedatos bd = new Basedatos();
+            string nTabla = "clientes";
+            
+            string condicion =  "idCliente = " + idCliente; 
+            bd.Eliminar(nTabla, condicion);
+
+            Globales.cargaGrid(sqlSelect,dgvClientes);
         }
     }
 }
