5afab62084acde0f6b84b7cd07b8a81b87a5a0ff
xisdesarrollos <rescamilla@xis.mx>
2015-11-03 11:40:51 -0600

    Edicion y eliminar

--- Seguros American/Forms/Clientes/FrmGestionClientes.Designer.cs
+++ Seguros American/Forms/Clientes/FrmGestionClientes.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.formFrameSkinner = new Elegant.Ui.FormFrameSkinner();
             this.btnMostrarTodos = new Elegant.Ui.Button();
             this.txtCriterio = new Elegant.Ui.TextBox();
@@ -35,8 +38,13 @@
             this.btnNuevo = new Elegant.Ui.Button();
             this.groupFiltro = new Elegant.Ui.GroupBox();
             this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.menuOpcionesClientes = new Elegant.Ui.ContextMenu(this.components);
+            this.contextMenuExtenderProvider1 = new Elegant.Ui.ContextMenuExtenderProvider(this.components);
+            this.btnEliminarCli = new Elegant.Ui.Button();
+            this.btnEditarCli = new Elegant.Ui.Button();
             this.groupFiltro.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuOpcionesClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // formFrameSkinner
@@ -110,17 +118,66 @@
             // 
             // dgvClientes
             // 
+            this.dgvClientes.AllowUserToAddRows = false;
+            this.dgvClientes.AllowUserToDeleteRows = false;
+            this.dgvClientes.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvClientes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.contextMenuExtenderProvider1.SetContextPopupMenu(this.dgvClientes, this.menuOpcionesClientes);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvClientes.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvClientes.Location = new System.Drawing.Point(14, 116);
             this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.ReadOnly = true;
+            this.dgvClientes.RowHeadersVisible = false;
+            this.dgvClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvClientes.Size = new System.Drawing.Size(736, 368);
             this.dgvClientes.TabIndex = 4;
+            this.dgvClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellContentClick);
+            // 
+            // menuOpcionesClientes
+            // 
+            this.menuOpcionesClientes.Items.AddRange(new System.Windows.Forms.Control[] {
+            this.btnEliminarCli,
+            this.btnEditarCli});
+            this.menuOpcionesClientes.KeepPopupsWithOffsetPlacementWithinPlacementArea = false;
+            this.menuOpcionesClientes.PlacementMode = Elegant.Ui.PopupPlacementMode.Bottom;
+            this.menuOpcionesClientes.PlacementOffset = new System.Drawing.Size(261, 106);
+            this.menuOpcionesClientes.Size = new System.Drawing.Size(100, 100);
+            // 
+            // btnEliminarCli
+            // 
+            this.btnEliminarCli.Id = "8f06857b-1a02-4390-b0c9-7628109f1d62";
+            this.btnEliminarCli.Location = new System.Drawing.Point(2, 2);
+            this.btnEliminarCli.Name = "btnEliminarCli";
+            this.btnEliminarCli.Size = new System.Drawing.Size(138, 23);
+            this.btnEliminarCli.TabIndex = 3;
+            this.btnEliminarCli.Text = "Eliminar";
+            this.btnEliminarCli.Click += new System.EventHandler(this.btnEliminarCli_Click);
+            // 
+            // btnEditarCli
+            // 
+            this.btnEditarCli.Id = "7c62275f-a764-4547-ac6b-a5739a532dab";
+            this.btnEditarCli.Location = new System.Drawing.Point(2, 25);
+            this.btnEditarCli.Name = "btnEditarCli";
+            this.btnEditarCli.Size = new System.Drawing.Size(138, 23);
+            this.btnEditarCli.TabIndex = 4;
+            this.btnEditarCli.Text = "Editar";
+            this.btnEditarCli.Click += new System.EventHandler(this.btnEditarCli_Click);
             // 
             // FrmGestionClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 500);
+            this.ClientSize = new System.Drawing.Size(776, 508);
             this.Controls.Add(this.dgvClientes);
             this.Controls.Add(this.groupFiltro);
             this.Name = "FrmGestionClientes";
@@ -129,6 +186,7 @@
             this.groupFiltro.ResumeLayout(false);
             this.groupFiltro.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menuOpcionesClientes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -142,5 +200,9 @@
         private Elegant.Ui.ComboBox cmbFiltro;
         private Elegant.Ui.Button btnMostrarTodos;
         private System.Windows.Forms.DataGridView dgvClientes;
+        private Elegant.Ui.ContextMenu menuOpcionesClientes;
+        private Elegant.Ui.Button btnEliminarCli;
+        private Elegant.Ui.Button btnEditarCli;
+        private Elegant.Ui.ContextMenuExtenderProvider contextMenuExtenderProvider1;
     }
 }
--- Seguros American/Forms/Clientes/FrmNuevoCliente.cs
+++ Seguros American/Forms/Clientes/FrmNuevoCliente.cs
@@ -14,39 +14,87 @@ namespace Seguros_American.Forms.Clientes
 {
     public partial class FrmNuevoCliente : Form
     {
+        private string idCliente;
+        private Basedatos bd = new Basedatos();
+
         public FrmNuevoCliente()
         {
             InitializeComponent();
         }
 
+        public FrmNuevoCliente(string id)
+        {
+            InitializeComponent();
+            this.idCliente = id;
+           
+        }
+
         private void FrmNuevoC_Load(object sender, EventArgs e)
         {
-
+            //here 
+            if (!Globales.EsNuevoCliente) {
+                cargarDatos(idCliente);
+            }
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
 
         }
+       
+        private void txtCiudad_TextChanged(object sender, EventArgs e)
+        {
 
+        }
+
+        private void btnGuardar_Click_1(object sender, EventArgs e)
+        {
+            //CLIENTE NUEVO
+            if (Globales.EsNuevoCliente) {
+                if (guardaCliente()) {
+                    MessageBox.Show("Se guardaron correctamente los datos del cliente");
+                    this.Close();
+                }
+                else {
+                    MessageBox.Show("Ocurrio un error al guardar los datos. Verifique con su proveedor");
+                }
+            }
+            //ACTUALIZAR CLIENTE
+            else {
+                if (actualizaCliente()) {
+                    MessageBox.Show("Se actualizaron correctamente los datos del cliente");
+                    this.Close();
+                }
+                else {
+                    MessageBox.Show("Ocurrio un error al actualizar los datos. Verifique con su proveedor");
+                }
+            }
+        }
+
+        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateNacimiento_Click(object sender, EventArgs e)
+        {
+         
+        }
         private bool guardaCliente()
         {
+            //guardar
             bool valorDeRetorno = true;
             try
             {
-
-
-                if (verificarValores())
-                {
-                    Basedatos db = new Basedatos();
+                if (verificarValores()) {
 
                     MySqlCommand cmd = new MySqlCommand();
-                    cmd.CommandText = "INSERT INTO clientes(nombre,rfcCliente,sexo, fechaNacimiento, calle, noExterior, noInterior, colonia, estado, ciudad, cp, pais, telefono, cel, email, fechaAlta, ocupacion, obs) " +
-                     "VALUES(@nombre,@rfcCliente, @sexo, @fechaNacimiento, @calle, @noExterior, @noInterior, @colonia, @estado, @ciudad, @cp, @pais, @telefono, @cel, @email, @fechaAlta, @ocupacion, @obs)";
+                    cmd.CommandText = "INSERT INTO clientes(nombre,rfcCliente,sexo, fechaNacimiento, calle, noExterior, noInterior, colonia, estado, ciudad, cp, pais, telefono, cel, email, fechaAlta, ocupacion, obs, noLicencia, estadoEmision) " +
+                         "VALUES(@nombre,@rfcCliente, @sexo, @fechaNacimiento, @calle, @noExterior, @noInterior, @colonia, @estado, @ciudad, @cp, @pais, @telefono, @cel, @email, @fechaAlta, @ocupacion, @obs, @noLicencia, @estadoEmision)";
                     cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
                     cmd.Parameters.AddWithValue("@rfcCliente", txtRfc.Text);
                     cmd.Parameters.AddWithValue("@sexo", cmbSexo.Text);
-                    cmd.Parameters.AddWithValue("@fechaNacimiento",  DateTime.Parse(dateNacimiento.Value.ToString()).ToString("yyyy-MM-dd"));//fecha de nacimiento genera edad actual.
+                    cmd.Parameters.AddWithValue("@fechaNacimiento", DateTime.Parse(dateNacimiento.Value.ToString()).ToString("yyyy-MM-dd"));//fecha de nacimiento genera edad actual.
                     cmd.Parameters.AddWithValue("@calle", txtCalle.Text);
                     cmd.Parameters.AddWithValue("@noExterior", txtNoE.Text);
                     cmd.Parameters.AddWithValue("@noInterior", txtNoI.Text);
@@ -62,30 +110,83 @@ namespace Seguros_American.Forms.Clientes
                     cmd.Parameters.AddWithValue("@fechaAlta", dateNow.ToString("yyyy-MM-dd HH:mm:ss")); //se toma del sistema
                     cmd.Parameters.AddWithValue("@ocupacion", txtOcupacion.Text);
                     cmd.Parameters.AddWithValue("@obs", txtBoxObs.Text);
-                    db.Insertar(cmd);
-                }
-                else
-                {
+                    cmd.Parameters.AddWithValue("@noLicencia", txtLicencia.Text);
+                    cmd.Parameters.AddWithValue("@estadoEmision", txtEstado.Text);
+                    bd.Insertar(cmd);
+                    
+                } 
+                else {
                     valorDeRetorno = false;
                 }
-
-
             }
             catch (MySqlException e)
             {
                 valorDeRetorno = false;
-                Console.WriteLine(e);
-   
+                Console.WriteLine(e);//debug
             }
             return valorDeRetorno;
         }
+       
+        private bool actualizaCliente()
+        {
+            try {
+                if (verificarValores())
+                {
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = "UPDATE clientes " +
+                        "SET nombre=@nombre, rfcCliente=@rfcCliente, sexo=@sexo, fechaNacimiento=@fechaNacimiento, " +
+                        "calle= @calle, noExterior=@noExterior, noInterior=@noInterior, colonia=@colonia, " +
+                        "estado=@estado, ciudad= @ciudad, cp=@cp, pais=@pais, telefono=@telefono, cel=@cel, " +
+                        "email=@email, fechaAlta=@fechaAlta, ocupacion=@ocupacion, obs= @obs, noLicencia= @noLicencia, " +
+                        "estadoEmision=@estadoEmision " + 
+                        "WHERE idCliente = @idCliente";
+                    
+                    cmd.Parameters.AddWithValue("@idCliente", this.idCliente);   
+                    cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                    cmd.Parameters.AddWithValue("@rfcCliente", txtRfc.Text);
+                    cmd.Parameters.AddWithValue("@sexo", cmbSexo.Text);
+                    cmd.Parameters.AddWithValue("@fechaNacimiento", DateTime.Parse(dateNacimiento.Value.ToString()).ToString("yyyy-MM-dd"));//fecha de nacimiento genera edad actual.
+                    cmd.Parameters.AddWithValue("@calle", txtCalle.Text);
+                    cmd.Parameters.AddWithValue("@noExterior", txtNoE.Text);
+                    cmd.Parameters.AddWithValue("@noInterior", txtNoI.Text);
+                    cmd.Parameters.AddWithValue("@colonia", txtColonia.Text);
+                    cmd.Parameters.AddWithValue("@estado", txtEstado.Text);
+                    cmd.Parameters.AddWithValue("@ciudad", txtCiudad.Text);
+                    cmd.Parameters.AddWithValue("@cp", txtCp.Text);
+                    cmd.Parameters.AddWithValue("@pais", cmbPais.Text);
+                    cmd.Parameters.AddWithValue("@telefono", txtTel.Text);
+                    cmd.Parameters.AddWithValue("@cel", txtBoxCel.Text);
+                    cmd.Parameters.AddWithValue("@email", txtCorreo.Text);
+                    DateTime dateNow = DateTime.Now;
+                    cmd.Parameters.AddWithValue("@fechaAlta", dateNow.ToString("yyyy-MM-dd HH:mm:ss")); //se toma del sistema
+                    cmd.Parameters.AddWithValue("@ocupacion", txtOcupacion.Text);
+                    cmd.Parameters.AddWithValue("@obs", txtBoxObs.Text);
+                    cmd.Parameters.AddWithValue("@noLicencia", txtLicencia.Text);
+                    cmd.Parameters.AddWithValue("@estadoEmision", txtEdo.Text);
+                    bd.Actualizar(cmd);
 
-        private bool verificarValores() {
+                }
+                else
+                {
+                    MessageBox.Show("Verifique campos vacios");
+                    return false;
+                }
+            }
+            catch (MySqlException e)
+            {
+                
+                Console.WriteLine(e);//debug
+                return false;
+            }
+            return true;
+        }
+        private bool verificarValores()
+        {
             bool value = true;
             //verificar campos del form...
             //nombre,rfcCliente,sexo, edad, calle, noExterior, noInterior, colonia, estado, 
             //cuidad, cp, pais, telefono, cel, email, fechaAlta, ocupacion, obs
-            if (string.IsNullOrEmpty(txtNombre.Text) ||string.IsNullOrEmpty(txtRfc.Text) ||
+            if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtRfc.Text) ||
                 string.IsNullOrEmpty(cmbSexo.Text) || string.IsNullOrEmpty(txtEdad.Text) ||
                 string.IsNullOrEmpty(txtCalle.Text) || string.IsNullOrEmpty(txtNoE.Text) ||
                 string.IsNullOrEmpty(txtColonia.Text) || string.IsNullOrEmpty(txtEstado.Text) ||
@@ -100,36 +201,53 @@ namespace Seguros_American.Forms.Clientes
             //verificar edad.
             //verificar 
             return value;
-
-        }
-        private void txtCiudad_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
-        private void btnGuardar_Click_1(object sender, EventArgs e)
+        private void cargarDatos(String id)
         {
-            if (guardaCliente())
+            //obteber la info de la BD usando el id del usuario.
+            string filtro = "*";
+            string condicion = "idCliente = " + id;
+            string tabla = "clientes";
+            
+            try
             {
-                MessageBox.Show("Se guardaron correctamente los datos del cliente");
-                this.Close();
+                DataTable dataTable = bd.Consultar(filtro, tabla, condicion);
+                dataTable.Rows[0][1].ToString();
+                //llenar el formulario con esa info.
+                txtNombre.Text      = dataTable.Rows[0][1].ToString(); 
+                txtRfc.Text         = dataTable.Rows[0][2].ToString();
+                cmbSexo.Text        = dataTable.Rows[0][3].ToString();
+                //DateTime.Parse(dateNacimiento.Value.ToString()).ToString("yyyy-MM-dd"));//fecha de nacimiento genera edad actual.
+                dateNacimiento.Text = dataTable.Rows[0][4].ToString();
+                txtCalle.Text       = dataTable.Rows[0][5].ToString(); 
+                txtNoE.Text         = dataTable.Rows[0][6].ToString(); 
+                txtNoI.Text         = dataTable.Rows[0][7].ToString(); 
+                txtColonia.Text     = dataTable.Rows[0][8].ToString();
+                txtEstado.Text      = dataTable.Rows[0][9].ToString(); 
+                txtCiudad.Text      = dataTable.Rows[0][10].ToString(); 
+                txtCp.Text          = dataTable.Rows[0][11].ToString(); 
+                cmbPais.Text        = dataTable.Rows[0][12].ToString(); 
+                txtTel.Text         = dataTable.Rows[0][13].ToString(); 
+                txtBoxCel.Text      = dataTable.Rows[0][14].ToString(); 
+                txtCorreo.Text      = dataTable.Rows[0][15].ToString(); 
+                //DateTime dateNow = DateTime.Now;
+                //dateNow.ToString("yyyy-MM-dd HH:mm:ss")); //se toma del sistema
+                txtOcupacion.Text   = dataTable.Rows[0][17].ToString(); 
+                txtBoxObs.Text      = dataTable.Rows[0][18].ToString();
+                txtLicencia.Text    = dataTable.Rows[0][19].ToString();
+                txtEdo.Text              = dataTable.Rows[0][20].ToString();
             }
-            else
+            catch (MySqlException e)
             {
-                MessageBox.Show("Verifique con su proveedor");
+                Console.WriteLine(e);//debug
             }
         }
-
-        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        
+        private bool actualizarCliente()
         {
-
+            return false;
         }
-
-        private void dateNacimiento_Click(object sender, EventArgs e)
-        {
-         
-        }
-
         private void dateNacimiento_ValueChanged(object sender, EventArgs e)
         {
             //cambiar edad automaticamente.
