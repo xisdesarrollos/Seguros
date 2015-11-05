b80710b51cac85cbf7c9b7d20cdb5e18af1cb26d
xisdesarrollos <rescamilla@xis.mx>
2015-11-05 15:43:30 -0600

    FRM

--- Seguros American/Forms/Clientes/FrmGestionClientes.Designer.cs
+++ Seguros American/Forms/Clientes/FrmGestionClientes.Designer.cs
@@ -57,7 +57,7 @@
             this.btnMostrarTodos.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnMostrarTodos.Id = "7d6d16c6-7730-4a31-9037-f4b468099530";
             this.btnMostrarTodos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMostrarTodos.Location = new System.Drawing.Point(911, 18);
+            this.btnMostrarTodos.Location = new System.Drawing.Point(814, 18);
             this.btnMostrarTodos.Name = "btnMostrarTodos";
             this.btnMostrarTodos.Size = new System.Drawing.Size(83, 41);
             this.btnMostrarTodos.TabIndex = 2;
@@ -72,7 +72,7 @@
             this.txtCriterio.LabelText = "Criterio:";
             this.txtCriterio.Location = new System.Drawing.Point(424, 28);
             this.txtCriterio.Name = "txtCriterio";
-            this.txtCriterio.Size = new System.Drawing.Size(76, 21);
+            this.txtCriterio.Size = new System.Drawing.Size(0, 21);
             this.txtCriterio.TabIndex = 1;
             this.txtCriterio.TextEditorWidth = 361;
             this.txtCriterio.TextChanged += new System.EventHandler(this.txtCriterio_TextChanged);
@@ -99,7 +99,7 @@
             this.btnNuevo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnNuevo.Id = "504c2201-c57b-49d8-9ab3-aab6f73b6a0a";
             this.btnNuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnNuevo.Location = new System.Drawing.Point(1042, 16);
+            this.btnNuevo.Location = new System.Drawing.Point(945, 16);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Size = new System.Drawing.Size(120, 45);
             this.btnNuevo.TabIndex = 3;
@@ -109,6 +109,8 @@
             // 
             // groupFiltro
             // 
+            this.groupFiltro.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupFiltro.AutoSize = true;
             this.groupFiltro.Controls.Add(this.btnNuevo);
             this.groupFiltro.Controls.Add(this.txtCriterio);
             this.groupFiltro.Controls.Add(this.cmbFiltro);
@@ -116,7 +118,7 @@
             this.groupFiltro.Id = "c0a9dae8-1b03-4cb4-84ea-645d02d27465";
             this.groupFiltro.Location = new System.Drawing.Point(34, 13);
             this.groupFiltro.Name = "groupFiltro";
-            this.groupFiltro.Size = new System.Drawing.Size(1203, 91);
+            this.groupFiltro.Size = new System.Drawing.Size(1106, 91);
             this.groupFiltro.TabIndex = 12;
             // 
             // dgvClientes
@@ -127,6 +129,7 @@
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.Azure;
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dgvClientes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvClientes.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.contextMenuExtenderProvider1.SetContextPopupMenu(this.dgvClientes, this.menuOpcionesClientes);
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -142,7 +145,7 @@
             this.dgvClientes.ReadOnly = true;
             this.dgvClientes.RowHeadersVisible = false;
             this.dgvClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvClientes.Size = new System.Drawing.Size(1203, 504);
+            this.dgvClientes.Size = new System.Drawing.Size(1103, 420);
             this.dgvClientes.TabIndex = 4;
             this.dgvClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellContentClick);
             // 
@@ -178,7 +181,9 @@
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(1147, 649);
+            this.btnOk.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOk.AutoSize = true;
+            this.btnOk.Location = new System.Drawing.Point(1047, 546);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(90, 68);
             this.btnOk.TabIndex = 13;
@@ -190,10 +195,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1141, 618);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1149, 626);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.dgvClientes);
             this.Controls.Add(this.groupFiltro);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
             this.Name = "FrmGestionClientes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestión de Clientes";
@@ -204,6 +211,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.menuOpcionesClientes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
